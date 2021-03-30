79df312ac4f24dc432e15669891995108dd82924
Windy Choi <swaf.korea@gmail.com>
2021-03-30 11:07:16 +0900

    icon & color

--- OMSWeb/ClientApp/src/app/modules/shared/navs/gnb-actions.component.ts
+++ OMSWeb/ClientApp/src/app/modules/shared/navs/gnb-actions.component.ts
@@ -3,29 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'oms-gnb-actions',
   templateUrl: './gnb-actions.component.html',
-  styles: [
-    `
-      .ai {
-        background-color: var(--theme-text-color);
-        color: #fff;
-        padding: 0;
-        min-width: 28px;
-        height: 28px;
-        line-height: 22px;
-        margin-right: 20px;
-      }
-      .ai .rect {
-        width: 22px;
-        height: 22px;
-        border: 1px solid white;
-        line-height: 22px;
-      }
-
-      .ai.active {
-        background-color: var(--theme-color-active);
-      }
-    `,
-  ],
+  styleUrls: ['./gnb-actions.component.scss'],
 })
 export class GnbActionsComponent implements OnInit {
   constructor() {}
