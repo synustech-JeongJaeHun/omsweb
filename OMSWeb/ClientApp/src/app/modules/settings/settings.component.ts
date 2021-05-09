5bfe3eac767a7d89979a3522a9f5de23e58cfe53
Windy Choi <swaf.korea@gmail.com>
2021-05-09 22:53:03 +0900

    vehicle tooltip

--- OMSWeb/ClientApp/src/app/modules/settings/settings.component.ts
+++ OMSWeb/ClientApp/src/app/modules/settings/settings.component.ts
@@ -26,7 +26,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this._dlg = this.dialog.open(SettingsDialogComponent, {
-      width: '900px',
+      width: '800px',
       hasBackdrop: false,
       disableClose: true,
       closeOnNavigation: true,
