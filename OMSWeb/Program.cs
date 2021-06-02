5d6deb3449a02e261fbd6c24540365123b981b4b
DESKTOP-32OQP0S\enkih <ygchoi@daimresearch.com>
2021-06-02 16:50:04 +0900

    # support remote access

--- OMSWeb/Program.cs
+++ OMSWeb/Program.cs
@@ -18,6 +18,7 @@ namespace OMSWeb
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureWebHost(x => x.UseUrls("http://0.0.0.0:5000"))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
