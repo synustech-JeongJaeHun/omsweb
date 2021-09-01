1a0fe783634d9b96a8bae008964c3490c9e78f5a
ygchoi_daim <ygchoi@daimresearch.com>
2021-09-01 15:35:20 +0900

    Fix #178 Support Remove Vehicle from oms via Mqtt command

--- OMSWeb/ClientApp/src/app/modules/controls/vehicles/map-update.component.ts
+++ OMSWeb/ClientApp/src/app/modules/controls/vehicles/map-update.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { DxDataGridComponent } from 'devextreme-angular';
 import DataSource from 'devextreme/data/data_source';
+import { AuthService } from '../../../services/auth.service';
 import { DialogService } from '../../../services/dialog.service';
+import { MessagesService } from '../../../services/messages.service';
 import { SystemsService } from '../../../services/systems.service';
 
 @Component({
@@ -10,13 +13,24 @@ import { SystemsService } from '../../../services/systems.service';
   styleUrls: ['./map-update.component.scss'],
 })
 export class MapUpdateComponent implements OnInit {
+  dataGrid: DxDataGridComponent;
   dataSource: DataSource;
   selectedIds: number[] = [];
 
+  get hasControlAccess(): boolean {
+    return this.auth.isAuthenticated;
+  }
+
+  get canControl(): boolean {
+    return this.selectedIds.length > 0;
+  }
+
   constructor(
+    private auth: AuthService,
     private systemSvc: SystemsService,
     private dialogSvc: DialogService,
-    private t$: TranslateService
+    private messageSvc: MessagesService,
+    private t$: TranslateService,
   ) {
     this.dataSource = this.systemSvc.vehicles();
   }
@@ -32,6 +46,18 @@ export class MapUpdateComponent implements OnInit {
       .subscribe((ok) => {
         if (ok) {
           // @TODO call update
+        }
+      });
+  }
+  onRemoveVehicle() {
+    if (!this.canControl) return;
+    this.dialogSvc
+      .confirm({ body: this.t$.instant('messages.confirmCommand') })
+      .subscribe((ok) => {
+        if (ok) {
+          this.messageSvc
+            .sendVehicleDirectCommand({ action: 'remove' }, this.selectedIds)
+            .subscribe();
         }
       });
   }
