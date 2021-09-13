5accd7b14cd99ab2d52704e88b2bb29f399d39c5
ygchoi_daim <ygchoi@daimresearch.com>
2021-09-13 13:39:24 +0900

    Vehicle Logical Id 표시 on Monitor / Status => vid 대신 vehicle online name (logical id) 표시

--- OMSWeb/ClientApp/src/app/modules/track-map/viewer/viewer-helper.ts
+++ OMSWeb/ClientApp/src/app/modules/track-map/viewer/viewer-helper.ts
@@ -7187,8 +7187,7 @@ export class ViewController {
     vehicle_scale: { scale: number; value: number }
   ) {
     let current_vehicle_label = d3_this.select('.label');
-    //let label = logicalId ? logicalId : id;
-    let label = id;
+    let label = logicalId ? logicalId : id;
     if (
       current_vehicle_label.nodes().length === 0 &&
       vehicle_scale.scale >= 0.6
@@ -11250,7 +11249,7 @@ export class ViewController {
           return y;
         })
         .html(function () {
-          let id = layout_object.id;
+          let id = layout_object.logicalId ? layout_object.logicalId : layout_object.id;
           if (overlap_adjustment) {
             return `Vehicle ${id}`;
           } else {
