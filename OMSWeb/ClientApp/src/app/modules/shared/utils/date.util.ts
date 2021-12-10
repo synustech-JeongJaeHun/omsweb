442003ef331ef3d0946210f61eaf3763204e7bad
kimjuho_daim <kimjuho@daimresearch.com>
2021-12-10 11:43:18 +0900

    refactoring data.util

--- OMSWeb/ClientApp/src/app/modules/shared/utils/date.util.ts
+++ OMSWeb/ClientApp/src/app/modules/shared/utils/date.util.ts
@@ -1,14 +1,5 @@
-import * as moment from 'moment';
-
 export namespace DateUtil {
   export const DateFormat = 'yyyy-MM-dd';
   export const TimeFormat = 'HH:mm:ss';
-  export const TimeFormatShort = 'HH:mm';
   export const DateTimeFormat = `${DateFormat} ${TimeFormat}`;
-  export const DateTimeFormatShort = `${DateFormat} ${TimeFormatShort}`;
-
-  export const toISOString = (date: string | Date): string => {
-    if (date instanceof Date) return date.toISOString();
-    else return moment(date).toISOString();
-  };
 }
