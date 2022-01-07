0ab7506d091b208067f8161c1d0a9e6d34e6c70d
kimjuho_daim <kimjuho@daimresearch.com>
2022-01-07 18:19:02 +0900

    2022-01-07 END
    todo
    [ ] check width, height props work
    [ ] check backdrop event emitting in setup method

--- OMSWeb/oms-track-monitor/src/OmsTrackMonitor.ce.vue
+++ OMSWeb/oms-track-monitor/src/OmsTrackMonitor.ce.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+import { ViewModes } from './legacies/Enums'
 import { IOmsTrackMonitor } from './legacies/IOmsTrackMonitor'
-import { UiStates } from './legacies/models/setting.model'
+import { IPreferences, UiStates } from './legacies/models/setting.model'
+import { MapDataService } from './legacies/serivces/MapData.service'
+import { MapStatesService } from './legacies/serivces/MapStates.service'
 import { makeFsProxy } from './utils/devMode'
 
 const props = defineProps<{
-  text: string
+  width: number,
+  height: number,
 }>()
 
 const emit = defineEmits<{
@@ -13,19 +17,39 @@ const emit = defineEmits<{
   (e: 'blue', value: { B: number }): void
 }>()
 
+// State Start
+
+const viewMode = ref<ViewModes>('PUBLIC')
+const permissions = reactive({
+  canManageOrders: false,
+  canManageVehicles: false,
+  canManageDisplaySettings: false
+})
+const preferences = ref<IPreferences>()
+const services = reactive<{
+  mapDataService?: MapDataService
+  mapStatesService?: MapStatesService
+}>({})
+
+// State End
+
 const exposed: IOmsTrackMonitor = {
   construct: function (
     mode,
-    trackSvgId,
-    minimapSvgId,
+    trackSvgId, // discard
+    minimapSvgId, // discard
     dataSvc,
     stateSvc
-  ) {},
-  adjust_floaters: function () {},
-  applyAfterSnapshotUpdated: function (updatedPropList) {},
-  applyUpdatedExpectedPath: function () {},
-  create_track: function (data) {},
-  destroy: function () {},
+  ) {
+    viewMode.value = mode
+    services.mapDataService = dataSvc
+    services.mapStatesService = stateSvc
+  },
+  adjust_floaters: function () { },
+  applyAfterSnapshotUpdated: function (updatedPropList) { },
+  applyUpdatedExpectedPath: function () { },
+  create_track: function (data) { },
+  destroy: function () { },
   get_selected_objects: function (object_type) {
     return []
   },
@@ -42,60 +66,65 @@ const exposed: IOmsTrackMonitor = {
     group_type,
     highlight_type,
     operation_type
-  ) {},
-  init_selection: function (is_clear_sel_objects) {},
-  onChangeConfig: function (event) {},
-  onChangeVisibility: function (event) {},
-  onCommandAction: function (event) {},
-  setUiStates: function (zoomInUiStates) {},
+  ) { },
+  init_selection: function (is_clear_sel_objects) { },
+  onChangeConfig: function (event) { },
+  onChangeVisibility: function (event) { },
+  onCommandAction: function (event) { },
+  setUiStates: function (zoomInUiStates) { },
   setup: function (
-    preferences,
+    paramPreferences,
     can_manage_orders,
     can_manage_vehicles,
     can_modify_display_settings
-  ) {},
+  ) {
+    preferences.value = paramPreferences
+    permissions.canManageOrders = can_manage_orders ?? false
+    permissions.canManageVehicles = can_manage_vehicles ?? false
+    permissions.canManageDisplaySettings = can_modify_display_settings ?? false
+  },
   update_buffers: function (
     update_list,
     is_apply_history,
     is_apply_revert
-  ) {},
+  ) { },
   update_clusters: function (
     update_list,
     is_apply_history,
     is_apply_revert
-  ) {},
+  ) { },
   update_disable_segment: function (
     data,
     operation,
     disabled_segment_id,
     is_skip_rendering // NOTE default false
-  ) {},
+  ) { },
   update_groups: function (
     update_list,
     is_apply_history,
     is_apply_revert
-  ) {},
+  ) { },
   update_mtls: function (
     update_list,
     is_apply_history,
     is_apply_revert
-  ) {},
+  ) { },
   update_segment_svg: function (
     data,
     dom_css, // NOTE not using in function
     excluded_segments,
     is_path_change
-  ) {},
+  ) { },
   update_segments: function (
     update_list,
     is_apply_history,
     is_apply_revert
-  ) {},
+  ) { },
   update_stations: function (
     update_list,
     is_apply_history,
     is_apply_revert
-  ) {},
+  ) { },
   update_vehicles: function (
     raw_data,
     operation,
@@ -116,13 +145,12 @@ function emitB(event: any) {
 </script>
 
 <template>
-  <button type="button" @click="emit('apple', 'A')">A</button>
-  <button type="button" @click="emitB($event)">B</button>
-  <input type="text" v-model="props.text" />
+  <div></div>
 </template>
 
 <style scoped>
-button {
-  margin-left: 100px;
+div {
+  width: v-bind("props.width");
+  width: v-bind("props.height");
 }
 </style>
