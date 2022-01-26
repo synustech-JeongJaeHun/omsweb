fc6bc447b85d498942261875399e9568b01f86af
kimjuho_daim <kimjuho@daimresearch.com>
2022-01-26 17:50:27 +0900

    group displaying 4/5 - mtl

--- OMSWeb/oms-track-monitor/src/mtl/components/Mtl.ce.vue
+++ OMSWeb/oms-track-monitor/src/mtl/components/Mtl.ce.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
 import { Mtl } from '../types/Mtl'
 import RasterizedText from '../../map/components/RasterizedText.ce.vue';
-import { computed } from 'vue';
+import { toRef } from 'vue';
 import { usePointPoisiton } from '../../point/points';
+import { useGroupColor } from '../../group/groups';
 
 const props = defineProps<{
   mtl: Mtl
 }>()
 
-const position = usePointPoisiton(computed(() => props.mtl.pointId))
+const position = usePointPoisiton(toRef(props.mtl, 'pointId'))
+const groupColor = useGroupColor('mtl', toRef(props.mtl, 'id'))
 
 </script>
 
 <template>
   <svg class="overflow-visible cursor-pointer" :x="position.x" :y="position.y">
-    <use href="#mtl" />
-    <RasterizedText y="70" :text="props.mtl.logicalId" />
+    <use
+      v-show="groupColor"
+      href="#mtl"
+      class="group-shadow"
+      :stroke="groupColor"
+      stroke-width="15"
+    />
+    <use href="#mtl" stroke="black" stroke-width="5" />
+    <RasterizedText class="invert" y="70" :text="props.mtl.logicalId" />
   </svg>
 </template>
