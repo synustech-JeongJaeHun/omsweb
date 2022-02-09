dc5b3fbb4adaa00a83d4b7a8716c0541fd233c1d
kimjuho_daim <kimjuho@daimresearch.com>
2022-02-09 12:50:23 +0900

    use requestAnimationFrame for order lines of vehicle

--- OMSWeb/oms-track-monitor/src/vehicle/components/Vehicle.ce.vue
+++ OMSWeb/oms-track-monitor/src/vehicle/components/Vehicle.ce.vue
@@ -4,10 +4,12 @@ import RasterizedText from '../../map/components/RasterizedText.ce.vue';
 import { findPointById } from '../../point/points';
 import { reactive, ref, toRef, watch } from 'vue';
 import { findSegmentByPoints } from '../../segment/segments';
-import { getPositionFromD } from '../../utils/svg/path';
+import { createPathElement, getPositionFromD } from '../../utils/svg/path';
 import { Segment } from '../../segment/types/Segment';
 import { encodeCommandsToD, moveTo, slicePathCommands } from '../../utils/svg/pathSegment';
 import { useGroupColor } from '../../group/groups';
+import { D } from '../../types/D';
+import MapReverseRotate from '../../rotate/components/MapReverseRotate.ce.vue';
 
 const props = defineProps<{
   vehicle: Vehicle
@@ -16,13 +18,17 @@ const props = defineProps<{
 const
   currentPosition = reactive({ x: 0, y: 0 }),
   currentSegment = ref<Segment>()
+
 const
   beforePosition = reactive({ x: 0, y: 0 }),
   beforeSegment = ref<Segment>()
 
 const
-  animateMotionRef = ref<SVGAnimateMotionElement>(),
-  animateMotionPath = ref('M 0 0')
+  realtimePosition = reactive({ x: 0, y: 0 })
+
+// const
+// animateMotionRef = ref<SVGAnimateMotionElement>(),
+// animateMotionPath = ref('M 0 0')
 
 watch(() => props.vehicle.lastUpdated, () => {
   const segment = findSegmentByPoints(props.vehicle.curPoint, props.vehicle.nextPoint)
@@ -41,6 +47,10 @@ watch(() => props.vehicle.lastUpdated, () => {
   currentPosition.y = y
   currentSegment.value = segment ?? currentSegment.value
 
+  // v => realtime
+  realtimePosition.x = x
+  realtimePosition.y = y
+
   const pathCommands = (function () {
     if (props.vehicle.updateType === 'AnimationIn2Segments' && currentSegment.value && beforeSegment.value) {
       const concatenatedCommands = [
@@ -56,12 +66,45 @@ watch(() => props.vehicle.lastUpdated, () => {
     const currentPositionPathCommands = [moveTo(currentPosition)]
     if (props.vehicle.updateType === 'NoAnimation')
       return currentPositionPathCommands
+
     return currentPositionPathCommands
   })()
 
-  animateMotionPath.value = encodeCommandsToD(pathCommands)
-  animateMotionRef.value?.beginElement()
+  const d = encodeCommandsToD(pathCommands)
+
+  // animateMotionPath.value = d
+  // animateMotionRef.value?.beginElement()
+  if (props.vehicle.updateType !== 'NoAnimation')
+    trackVehiclePosition(d)
 })
+
+function trackVehiclePosition(d: D) {
+  const pathElement = createPathElement(d)
+  const totalLength = pathElement.getTotalLength()
+  // https://developer.mozilla.org/ko/docs/Web/API/Performance/now
+  const startTime = performance.now()
+
+  function step(now: DOMHighResTimeStamp) {
+    // time with microsecond
+    // animation duration 0.3s with linear
+    const diff = now - startTime
+    if (diff > 300) {
+      const endPosition = pathElement.getPointAtLength(totalLength)
+      realtimePosition.x = endPosition.x
+      realtimePosition.y = endPosition.y
+      return
+    }
+
+    const position = pathElement.getPointAtLength(totalLength / 300 * diff)
+    realtimePosition.x = position.x
+    realtimePosition.y = position.y
+
+    globalThis.requestAnimationFrame(step)
+  }
+
+  // https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
+  globalThis.requestAnimationFrame(step)
+}
 
 const groupColor = useGroupColor('vehicle', toRef(props.vehicle, 'id'))
 
@@ -72,9 +115,22 @@ const groupColor = useGroupColor('vehicle', toRef(props.vehicle, 'id'))
     <circle v-show="groupColor" class="group-shadow" r="120" :fill="groupColor" />
     <circle r="80" fill="none" stroke="red" stroke-width="20" />
     <!-- <text y="70">{{ props.vehicle.logicalId }}</text> -->
-    <RasterizedText class="invert" y="100" :text="props.vehicle.logicalId" />
+    <MapReverseRotate>
+      <RasterizedText class="invert" y="100" :text="props.vehicle.logicalId" />
+    </MapReverseRotate>
   </symbol>
-  <use :href="`#vehicle-${props.vehicle.id}`">
-    <animateMotion ref="animateMotionRef" fill="freeze" dur="0.3s" :path="animateMotionPath" />
-  </use>
+
+  <use :href="`#vehicle-${props.vehicle.id}`" :x="realtimePosition.x" :y="realtimePosition.y" />
+  <!-- <animateMotion ref="animateMotionRef" fill="freeze" dur="0.3s" :path="animateMotionPath" /> -->
+
+  <line
+    :x1="realtimePosition.x"
+    :y1="realtimePosition.y"
+    x2="23000"
+    y2="30000"
+    stroke="blue"
+    stroke-width="22"
+    stroke-linecap="round"
+    marker-end="url(#vehicle-line-blue)"
+  />
 </template>
--- OMSWeb/oms-track-monitor/src/vehicle/components/VehicleLayer.ce.vue
+++ OMSWeb/oms-track-monitor/src/vehicle/components/VehicleLayer.ce.vue
@@ -6,6 +6,34 @@ import { vehicles } from '../vehicles'
 
 <template>
   <Layer>
+    <defs>
+      <marker
+        id="vehicle-line-green"
+        viewBox="0 0 10 10"
+        refX="1"
+        refY="5"
+        markerUnits="strokeWidth"
+        markerWidth="3"
+        markerHeight="3"
+        orient="auto"
+        fill="green"
+      >
+        <path d="M 0 0 L 10 5 L 0 10 z" />
+      </marker>
+      <marker
+        id="vehicle-line-blue"
+        viewBox="0 0 10 10"
+        refX="1"
+        refY="5"
+        markerUnits="strokeWidth"
+        markerWidth="3"
+        markerHeight="3"
+        orient="auto"
+        fill="blue"
+      >
+        <path d="M 0 0 L 10 5 L 0 10 z" />
+      </marker>
+    </defs>
     <Vehicle v-for="vehicle of vehicles" :key="vehicle.id" :vehicle="vehicle" />
   </Layer>
 </template>
