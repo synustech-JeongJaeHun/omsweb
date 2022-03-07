8ba63a560bd29035798cd66ad51e20ea365b6895
kimjuho_daim <kimjuho@daimresearch.com>
2022-03-07 15:27:57 +0900

    manual order dialog complete

--- OMSWeb/ClientApp/src/app/modules/shared/forms/unit-selector.component.ts
+++ OMSWeb/ClientApp/src/app/modules/shared/forms/unit-selector.component.ts
@@ -8,10 +8,10 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { TrackStatusService } from '@oms/root/services/track-status.service';
 import { Observable, of } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { ILookupUnit } from '../../../models/map.interface';
-import { TrackIdService } from '../../../services/track-id.service';
 
 @Component({
   selector: 'oms-unit-selector',
@@ -39,7 +39,7 @@ export class UnitSelectorComponent implements OnInit, OnChanges {
   inputControl = new FormControl();
   targetOptions$: Observable<ILookupUnit[]>;
 
-  constructor(private idSvc: TrackIdService) { }
+  constructor(private trackStatusService: TrackStatusService) { }
   ngOnChanges(changes: SimpleChanges): void {
     const { disabled, selectedUnit } = changes;
     if (disabled) {
@@ -57,7 +57,33 @@ export class UnitSelectorComponent implements OnInit, OnChanges {
     this.targetOptions$ = this.inputControl.valueChanges.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((value) => this.idSvc.lookupUnitsByLogicalId(this.findScopes, value))
+      // switchMap((value) => this.idSvc.lookupUnitsByLogicalId(this.findScopes, value))
+      switchMap((value) => {
+        const result: ILookupUnit[] = []
+        if (this.findScopes.includes('vehicles')) {
+          result.push(...this.trackStatusService.trackData.vehicles
+            .filter(v => v.logicalId.includes(value))
+            .map(v => ({ id: v.id, objectType: "Vehicle", logicalId: v.logicalId, physicalId: v.physicalId })))
+        }
+        if (this.findScopes.includes('points')) {
+          result.push(...this.trackStatusService.trackData.points
+            .filter(p => p.logicalId.includes(value))
+            .map(p => ({ id: p.id, objectType: "Point", logicalId: p.logicalId, physicalId: p.physicalId })))
+        }
+        if (this.findScopes.includes('stations')) {
+
+          result.push(...this.trackStatusService.trackData.stations
+            .filter(s => s.logicalId.includes(value))
+            .map(s => ({ id: s.id, objectType: "Station", logicalId: s.logicalId, physicalId: s.physicalId })))
+        }
+        if (this.findScopes.includes('buffers')) {
+          result.push(...this.trackStatusService.trackData.buffers
+            .filter(b => b.logicalId.includes(value))
+            .map(b => ({ id: b.id, objectType: "Buffer", logicalId: b.logicalId, physicalId: b.physicalId })))
+        }
+
+        return of(result)
+      })
     );
   }
   displayFn(item: ILookupUnit): string | undefined {
