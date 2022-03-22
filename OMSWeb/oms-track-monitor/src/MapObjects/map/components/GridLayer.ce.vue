cea956b835af899a66c6cfe3ef12a020bd9b92ff
kimjuho_daim <kimjuho@daimresearch.com>
2022-03-22 11:03:22 +0900

    remove frame border

--- OMSWeb/oms-track-monitor/src/MapObjects/map/components/GridLayer.ce.vue
+++ OMSWeb/oms-track-monitor/src/MapObjects/map/components/GridLayer.ce.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import { mapSizePropertiesInfo } from '../mapSizeProperties';
-import Layer from './Layer.ce.vue';
+import { mapSizePropertiesInfo } from '../mapSizeProperties'
+import Layer from './Layer.ce.vue'
 
-const
-  UnitLength = 3000,
-  LineStroke = "rgba(128,128,128, 0.2)",
+const UnitLength = 3000,
+  LineStroke = 'rgba(128,128,128, 0.2)',
   LineStrokeWidth = 5,
   MapMargin = 3000,
-  MapCornerStroke = "black",
+  MapCornerStroke = 'black',
   MapCornerStrokeWidth = 50
-
 </script>
 
 <template>
@@ -36,18 +34,18 @@ const
     </defs>
 
     <!-- pointer-events="none" for map backdrop event -->
+    <!-- :stroke="MapCornerStroke"
+      :stroke-width="MapCornerStrokeWidth" -->
     <rect
       pointer-events="none"
-      :x="(-1) * MapMargin"
-      :y="(-1) * MapMargin"
+      :x="-1 * MapMargin"
+      :y="-1 * MapMargin"
       fill="url(#mapGridPattern)"
-      :stroke="MapCornerStroke"
-      :stroke-width="MapCornerStrokeWidth"
-      :width="mapSizePropertiesInfo.maxX + (MapMargin * 2)"
-      :height="mapSizePropertiesInfo.maxY + (MapMargin * 2)"
+      :width="mapSizePropertiesInfo.maxX + MapMargin * 2"
+      :height="mapSizePropertiesInfo.maxY + MapMargin * 2"
     />
 
-    <circle id="originPoint" cx="0" cy="0" r="10" fill="rgba(0,0,0,0.5)" />
+    <!-- <circle id="originPoint" cx="0" cy="0" r="10" fill="rgba(0,0,0,0.5)" />
 
     <text x="0" y="0" text-anchor="middle" style="visibility: hidden; transform: scaleY(-1);">
       <animate
@@ -68,6 +66,6 @@ const
         dur="0.5s"
         fill="freeze"
       />Origin Point (0,0)
-    </text>
+    </text> -->
   </Layer>
 </template>
