07811dd7a84c2cbdb6a7a0ecf09f6f9b0c1580cb
kimjuho_daim <kimjuho@daimresearch.com>
2022-04-22 18:53:08 +0900

    playback - order history events - sql bug fix

--- OMSWeb/Repositories/PlaybackRepository.cs
+++ OMSWeb/Repositories/PlaybackRepository.cs
@@ -207,16 +207,16 @@ namespace OMSWeb.Repositories
                 t.table_name, 
                 jt.*, 
                 CASE
-                    WHEN OD.time_failed IS NOT NULL THEN 'FAILED'
-                    WHEN OD.time_aborted IS NOT NULL THEN 'ABORTED'
-                    WHEN OD.time_completed IS NOT NULL THEN 'COMPLETED'
-                    WHEN OD.time_unload_completed IS NOT NULL THEN 'UNLOADED'
-                    WHEN OD.time_unload_started IS NOT NULL THEN 'UNLOADING'
-                    WHEN OD.time_load_completed IS NOT NULL THEN 'LOADED'
-                    WHEN OD.time_load_started IS NOT NULL THEN 'LOADING'
-                    WHEN OD.time_vehicle_arrived IS NOT NULL THEN 'ARRIVED'
-                    WHEN OD.time_assigned IS NOT NULL THEN 'ASSIGNED'
-                    WHEN OD.time_assigned IS NULL THEN 'UNASSIGNED'    
+                    WHEN jt.time_failed IS NOT NULL THEN 'FAILED'
+                    WHEN jt.time_aborted IS NOT NULL THEN 'ABORTED'
+                    WHEN jt.time_completed IS NOT NULL THEN 'COMPLETED'
+                    WHEN jt.time_unload_completed IS NOT NULL THEN 'UNLOADED'
+                    WHEN jt.time_unload_started IS NOT NULL THEN 'UNLOADING'
+                    WHEN jt.time_load_completed IS NOT NULL THEN 'LOADED'
+                    WHEN jt.time_load_started IS NOT NULL THEN 'LOADING'
+                    WHEN jt.time_vehicle_arrived IS NOT NULL THEN 'ARRIVED'
+                    WHEN jt.time_assigned IS NOT NULL THEN 'ASSIGNED'
+                    WHEN jt.time_assigned IS NULL THEN 'UNASSIGNED'    
                 END AS state
             FROM timeline t join order_history jt ON t.event_id = jt.id
             WHERE t.event_time between @from AND @to AND table_name = 'order_history'
