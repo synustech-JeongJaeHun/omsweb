7a12569cd4bbdd130354dbda081b6ffc25bbf518
ygchoi_daim <ygchoi@daimresearch.com>
2022-05-05 15:40:49 +0900

    Modify VehicleTable's from,to,move as onlinename

--- OMSWeb/Repositories/QueryFactory.cs
+++ OMSWeb/Repositories/QueryFactory.cs
@@ -208,25 +208,96 @@ namespace OMSWeb.Repositories
             END AS isConnected, 
             CASE 
             WHEN OD.location_pickup IS NOT NULL AND OD.location_dropoff IS NOT NULL   -- FROM-TO order
-            THEN
-            CASE 
-                WHEN OD.time_vehicle_arrived IS NULL
-                THEN OD.location_pickup		                                      -- display FROM
-                ELSE OD.location_dropoff		                                      -- display To
-            END
+                THEN
+                    CASE 
+                        WHEN OD.time_vehicle_arrived IS NULL
+                        THEN OD.location_pickup		                                          -- display FROM
+                        ELSE OD.location_dropoff		                                      -- display To
+                    END
             WHEN OD.location_pickup IS NOT NULL AND OD.location_dropoff IS NULL       -- FROM order
-            THEN OD.location_pickup		                                      -- display FROM
+                THEN OD.location_pickup		                                          -- display FROM
             WHEN OD.location_pickup IS NULL AND OD.location_dropoff IS NOT NULL       -- TO order
-            THEN OD.location_dropoff		                                      -- display TO
+                THEN OD.location_dropoff		                                      -- display TO
             WHEN OD.location_move IS NOT NULL                                         -- MOVE order
-            THEN OD.location_move		                                      -- display MOVETO
+                THEN OD.location_move		                                          -- display MOVETO
             END AS command_point
-        FROM vehicles AS VH
-        LEFT OUTER JOIN orders AS OD
-        ON VH.order_id = OD.id AND OD.time_completed IS NULL AND OD.time_aborted IS NULL
-        --*user_id_condition*--AND VH.user_id = OD.user_id    
-        --*user_id_condition*--WHERE VH.user_id = @userId
-        ORDER BY VH.id
+
+            FROM vehicles AS VH
+            LEFT OUTER JOIN orders AS OD
+            ON VH.order_id = OD.id AND OD.time_completed IS NULL AND OD.time_aborted IS NULL
+            --*user_id_condition*--AND VH.user_id = OD.user_id    
+            --*user_id_condition*--WHERE VH.user_id = @userId
+            ORDER BY VH.id
+      "},
+       {"vehicleStates",  @"
+            SELECT
+            VH.id, VH.physical_id, VH.logical_id, VH.last_point AS cur_point, 
+            VH.moving_state, VH.map_db,
+            OD.id AS order_id,
+            CASE 
+                WHEN OD.location_pickup IS NOT NULL AND OD.location_dropoff IS NOT NULL        -- FROM-TO order
+                    THEN
+                CASE 
+                    WHEN OD.time_vehicle_arrived IS NULL THEN OD.location_pickup		                                        -- display FROM
+                    ELSE OD.location_dropoff		                                    -- display To
+                END
+                WHEN OD.location_pickup IS NOT NULL AND OD.location_dropoff IS NULL            -- FROM order
+                    THEN OD.location_pickup		                                                -- display FROM
+                WHEN OD.location_pickup IS NULL AND OD.location_dropoff IS NOT NULL            -- TO order
+                    THEN OD.location_dropoff		                                            -- display TO
+                WHEN OD.location_move IS NOT NULL                                              -- MOVE order
+                    THEN OD.location_move		                                                -- display MOVETO
+            END AS command_point,
+
+            --OD.location_pickup, 
+            --OD.location_dropoff, 
+            --OD.location_move,
+            CASE
+			    WHEN OD.location_pickup LIKE '%s%' THEN	(SELECT ST.logical_Id FROM stations ST WHERE concat('s', cast(ST.id as varchar)) = OD.location_pickup)
+			    WHEN OD.location_pickup LIKE '%b%' THEN	(SELECT ST.logical_Id FROM stations ST WHERE concat('b', cast(ST.id as varchar)) = OD.location_pickup)
+			    ELSE OD.location_pickup
+		    EnD AS location_pickup,
+		    CASE
+			    WHEN OD.location_dropoff LIKE '%s%' THEN	(SELECT ST.logical_Id FROM stations ST WHERE concat('s', cast(ST.id as varchar)) = OD.location_dropoff)
+			    WHEN OD.location_dropoff LIKE '%b%' THEN	(SELECT ST.logical_Id FROM stations ST WHERE concat('b', cast(ST.id as varchar)) = OD.location_dropoff)
+			    ELSE OD.location_dropoff
+		    EnD AS location_dropoff,
+		    CASE
+			    WHEN OD.location_move LIKE '%s%' THEN	(SELECT ST.logical_Id FROM stations ST WHERE concat('s', cast(ST.id as varchar)) = OD.location_move)
+			    WHEN OD.location_move LIKE '%b%' THEN	(SELECT ST.logical_Id FROM stations ST WHERE concat('b', cast(ST.id as varchar)) = OD.location_move)
+			    ELSE OD.location_move
+		    EnD AS location_move,
+
+            VH.cargo_state, 
+            CR.carrier_id AS CarrierLabel,
+            VH.mode,
+            CASE 
+                WHEN order_origin LIKE '%MCS%' THEN true 
+                WHEN order_origin LIKE '%*%' THEN true 
+                ELSE false
+            END As host_order,  
+            order_origin, can_be_pushed,
+            VH.is_sensor_stopped, VH.is_blocked,
+            CASE
+            WHEN LENGTH(VH.error_list) = 0 THEN '0' ELSE VH.error_list
+            END AS error_list,
+            VH.distance_total, VH.runtime_total, VH.type, VH.rail_in, VH.is_maint, 
+            CASE 
+                WHEN VH.connection = 0 THEN FALSE
+                WHEN VH.connection = 1 THEN TRUE
+                WHEN VH.connection = 2 THEN TRUE
+                WHEN VH.connection = 3 THEN FALSE
+                WHEN VH.connection IS NULL THEN FALSE
+            ENd AS isConnected, 
+            GO.group_id
+            FROM vehicles AS VH
+            LEFT OUTER JOIN orders AS OD
+                ON VH.order_id = OD.id AND OD.time_completed IS NULL AND OD.time_aborted IS NULL
+            LEFT JOIN grouped_objects AS GO 
+	            ON VH.id = GO.reference_id AND GO.reference_table = 'vehicle'
+            LEFT JOIN carriers AS CR 
+	            ON VH.logical_id = CR.carrier_location
+            ORDER BY VH.id
       "},
       {"orderStatus", @"
       SELECT --*order_condition*
