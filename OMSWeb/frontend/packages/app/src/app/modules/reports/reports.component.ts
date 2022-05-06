7ad5b9ba7ac97586e9eebfb9e7bc576c9d195681
kimjuho_daim <kimjuho@daimresearch.com>
2022-05-06 20:02:20 +0900

    bugfix - unmount styles when leaving report pages

--- OMSWeb/frontend/packages/app/src/app/modules/reports/reports.component.ts
+++ OMSWeb/frontend/packages/app/src/app/modules/reports/reports.component.ts
@@ -1,24 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	OnChanges,
+	OnDestroy,
+	AfterViewInit,
+} from '@angular/core'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from '../../../reactApp'
-import { ActivatedRoute } from "@angular/router";
-import { createBrowserHistory } from "history"
-const history = createBrowserHistory();
-
+import * as uuid from 'uuid'
+import { ActivatedRoute } from '@angular/router'
+import { createBrowserHistory } from 'history'
+const history = createBrowserHistory()
 @Component({
-  selector: 'oms-reports',
-  templateUrl: './reports.component.html',
-  styleUrls: ['reports.component.scss'],
+	selector: 'oms-reports',
+	templateUrl: './reports.component.html',
+	styleUrls: ['reports.component.scss'],
 })
-export class ReportsComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    const node = document.getElementById('react-root')
-		const rnode = React.createElement(App, { location: location }, null)
-		ReactDOM.render(rnode, node)
-  }
-
+export class ReportsComponent
+	implements OnInit, OnDestroy, OnChanges, AfterViewInit
+{
+	private rootDomID: string
+	constructor(private route: ActivatedRoute) {}
+	protected getRootDomNode() {
+		const node = document.getElementById('react-root')
+		return node
+	}
+	private isMounted(): boolean {
+		return !!this.rootDomID
+	}
+	protected render() {
+		if (this.isMounted()) {
+			const rnode = React.createElement(App, { location: location }, null)
+			ReactDOM.render(rnode, this.getRootDomNode())
+		}
+	}
+	ngOnInit() {
+		this.rootDomID = uuid.v1()
+	}
+	ngOnChanges() {
+		this.render()
+	}
+	ngAfterViewInit() {
+		this.render()
+	}
+	ngOnDestroy() {
+		ReactDOM.unmountComponentAtNode(this.getRootDomNode())
+	}
 }
--- OMSWeb/frontend/packages/app/src/reactApp/App.tsx
+++ OMSWeb/frontend/packages/app/src/reactApp/App.tsx
@@ -1,45 +1,57 @@
 import * as React from 'react'
 import * as R from 'ramda'
-import { GlobalStyle } from '@daimre/component-library'
 import styled from '@emotion/styled'
 import { NormalTr, Trend, AbnormalTr, Alarm } from './screens'
-
-const Wrapper = styled.div`
-  height: 100%;
+import { globalStyle } from '@daimre/styles'
+import createCache from '@emotion/cache'
+import { cache, css } from '@emotion/css'
+import { serializeStyles } from '@emotion/serialize'
+const globalThemeCache = createCache({ key: 'global-theme' })
+const themeStyles = (theme) => css`
+	${globalStyle}
 `
-
-
+function injectThemedGlobal(...args) {
+	const serialized = serializeStyles(args, cache.registered)
+	if (!globalThemeCache.inserted[serialized.name]) {
+		globalThemeCache.insert('', serialized, globalThemeCache.sheet, true)
+	}
+}
+function flushThemedGlobals() {
+	globalThemeCache.sheet.flush()
+	globalThemeCache.inserted = {}
+	globalThemeCache.registered = {}
+}
+const Wrapper = styled.div`
+	height: 100%;
+`
 const App: React.FC<Props> = ({ location }: Props) => {
-
-  const { pathname } = location
-  const path = R.toLower(pathname)
-
-  const renderPath = React.useCallback(() => {
-    switch (path) {
-      case '/reports/normaltr':
-        return <NormalTr />
-      case '/reports/kpi':
-        return <Trend />
-      case '/reports/abnormaltr':
-        return <AbnormalTr />
-      case '/reports/alarm':
-        return <Alarm />
-      default:
-        return <NormalTr />
-    }
-  }, [path])
-
-  return (
-    <Wrapper>
-      <GlobalStyle />
-      {renderPath()}
-    </Wrapper>
-
-  )
+	const { pathname } = location
+	const path = R.toLower(pathname)
+	React.useEffect(() => {
+		injectThemedGlobal`
+      ${themeStyles('global')}
+    `
+		return () => {
+			flushThemedGlobals()
+		}
+	}, [])
+	const renderPath = React.useCallback(() => {
+		switch (path) {
+			case '/reports/normaltr':
+				return <NormalTr />
+			case '/reports/kpi':
+				return <Trend />
+			case '/reports/abnormaltr':
+				return <AbnormalTr />
+			case '/reports/alarm':
+				return <Alarm />
+			default:
+				return <NormalTr />
+		}
+	}, [path])
+	return <Wrapper>{renderPath()}</Wrapper>
 }
-
 interface Props {
-  location: any
+	location: any
 }
-
 export default App
