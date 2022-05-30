eea61e4a69b594226cc121ed9e6c0cf6112c8bc8
kimjuho_daim <kimjuho@daimresearch.com>
2022-05-30 10:03:59 +0900

    label changed: Not Selected => 0 (Default)

--- OMSWeb/frontend/packages/app/src/app/modules/track-map/viewer/map-viewer.component.ts
+++ OMSWeb/frontend/packages/app/src/app/modules/track-map/viewer/map-viewer.component.ts
@@ -519,7 +519,7 @@ export class MapViewerComponent implements OnInit, OnDestroy {
 	// point > home
 	homeAndGroupSelectList = [
 		{ value: 'OFF', label: 'OFF' },
-		{ value: 'No Group', label: 'Group: Not Selected' },
+		{ value: 'No Group', label: 'Group: 0 (Default)' },
 		...this.trackStatusService.trackData.groups
 			.map((g) => String(g.id))
 			.map((e) => ({ value: e, label: `Group: ${e}` })),
