4aada156ba32baf406d7c105de5d94bfd38e8751
kimjuho_daim <kimjuho@daimresearch.com>
2022-05-30 14:26:10 +0900

    complete home editing toggle view code

---
+++ OMSWeb/frontend/packages/app/src/app/modules/settings/preferences/system-preference.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'oms-system-preference',
+	templateUrl: './system-preference.component.html',
+	styleUrls: ['./system-preference.component.scss'],
+})
+export class SystemPreferenceComponent {
+	constructor() {}
+
+	public setHomeEditing(mode: 'disable' | 'enable') {
+		// todo
+	}
+}
