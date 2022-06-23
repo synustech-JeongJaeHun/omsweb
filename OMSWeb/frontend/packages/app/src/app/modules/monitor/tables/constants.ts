7ae694b156e0c06dd774b7c0a294d3b8cc760596
kimjuho_daim <kimjuho@daimresearch.com>
2022-06-23 17:50:49 +0900

    using repaintChangesOnly true

--- OMSWeb/frontend/packages/app/src/app/modules/monitor/tables/constants.ts
+++ OMSWeb/frontend/packages/app/src/app/modules/monitor/tables/constants.ts
@@ -1,3 +1,3 @@
-const AuditTimeDuration = 800 // in ms, 1000ms = 1s
+const AuditTimeDuration = 750 // in ms, 1000ms = 1s
 
 export { AuditTimeDuration }
--- OMSWeb/frontend/packages/app/src/app/services/status.service.ts
+++ OMSWeb/frontend/packages/app/src/app/services/status.service.ts
@@ -1,72 +1,81 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import * as AspNetData from 'devextreme-aspnet-data-nojquery';
-import DataSource from 'devextreme/data/data_source';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import * as AspNetData from 'devextreme-aspnet-data-nojquery'
+import DataSource from 'devextreme/data/data_source'
 
-import { Dto } from '@oms/models/dto/track.model';
+import { Dto } from '@oms/models/dto/track.model'
+
+// Document about DataGrid
+// https://js.devexpress.com/Demos/WidgetsGallery/Demo/DataGrid/RealTimeUpdates/Angular/Light/
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class StatusService {
-  private baseUrl = '/api/status';
-  constructor(private http: HttpClient) {}
+	private baseUrl = '/api/status'
+	constructor(private http: HttpClient) {}
 
-  getTrack(): Observable<Dto.ITrackData> {
-    return this.http.get<Dto.ITrackData>(`${this.baseUrl}/tracks`);
-  }
+	getTrack(): Observable<Dto.ITrackData> {
+		return this.http.get<Dto.ITrackData>(`${this.baseUrl}/tracks`)
+	}
 
-  getVehicles(): Observable<Dto.IVehicleTrackData> {
-    return this.http.get(`${this.baseUrl}/tracks/vehicles`);
-  }
+	getVehicles(): Observable<Dto.IVehicleTrackData> {
+		return this.http.get(`${this.baseUrl}/tracks/vehicles`)
+	}
 
-  orderStatusDataSource(): DataSource {
-    return new DataSource({
-      store: AspNetData.createStore({
-        key: 'id',
-        loadUrl: `${this.baseUrl}/orders`,
-      }),
-    });
-  }
-  vehicleStatusDataSource(): DataSource {
-    return new DataSource({
-      store: AspNetData.createStore({
-        key: 'id',
-        loadUrl: `${this.baseUrl}/vehicles`,
-      }),
-    });
-  }
-  stationStatusDataSource(): DataSource {
-    return new DataSource({
-      store: AspNetData.createStore({
-        key: 'id',
-        loadUrl: `${this.baseUrl}/stations`,
-      }),
-    });
-  }
-  bufferStatusDataSource(): DataSource {
-    return new DataSource({
-      store: AspNetData.createStore({
-        key: 'id',
-        loadUrl: `${this.baseUrl}/buffers`,
-      }),
-    });
-  }
-  zcuStatusDataSource(): DataSource {
-    return new DataSource({
-      store: AspNetData.createStore({
-        key: 'id',
-        loadUrl: `${this.baseUrl}/zcus`,
-      }),
-    });
-  }
-  clusterStatusDataSource(): DataSource {
-    return new DataSource({
-      store: AspNetData.createStore({
-        key: 'id',
-        loadUrl: `${this.baseUrl}/clusters`,
-      }),
-    });
-  }
+	orderStatusDataSource(): DataSource {
+		return new DataSource({
+			store: AspNetData.createStore({
+				key: 'id',
+				loadUrl: `${this.baseUrl}/orders`,
+			}),
+			reshapeOnPush: true,
+		})
+	}
+	vehicleStatusDataSource(): DataSource {
+		return new DataSource({
+			store: AspNetData.createStore({
+				key: 'id',
+				loadUrl: `${this.baseUrl}/vehicles`,
+			}),
+			reshapeOnPush: true,
+		})
+	}
+	stationStatusDataSource(): DataSource {
+		return new DataSource({
+			store: AspNetData.createStore({
+				key: 'id',
+				loadUrl: `${this.baseUrl}/stations`,
+			}),
+			reshapeOnPush: true,
+		})
+	}
+	bufferStatusDataSource(): DataSource {
+		return new DataSource({
+			store: AspNetData.createStore({
+				key: 'id',
+				loadUrl: `${this.baseUrl}/buffers`,
+			}),
+			reshapeOnPush: true,
+		})
+	}
+	zcuStatusDataSource(): DataSource {
+		return new DataSource({
+			store: AspNetData.createStore({
+				key: 'id',
+				loadUrl: `${this.baseUrl}/zcus`,
+			}),
+			reshapeOnPush: true,
+		})
+	}
+	clusterStatusDataSource(): DataSource {
+		return new DataSource({
+			store: AspNetData.createStore({
+				key: 'id',
+				loadUrl: `${this.baseUrl}/clusters`,
+			}),
+			reshapeOnPush: true,
+		})
+	}
 }
