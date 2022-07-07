92f3f9b26a0bd9132e3ca9dd1a8307d0a63aa7df
ygchoi_daim <ygchoi@daimresearch.com>
2022-07-07 18:10:30 +0900

    add FireShutter Model

---
+++ OMSWeb/Models/Entities/FireShutterStatusEntity.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace OMSWeb.Models.Entities
+{
+    public class FireShutterStatusEntity
+    {
+        public int Id { get; set; }
+
+        public string logicalId { get; set; }
+
+        public int status { get; set; }
+
+        public string statusMsg { get; set; }
+
+    }
+}
