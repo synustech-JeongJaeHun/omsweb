351834a96e7b731bb5219c46951c5f749cee8511
이동윤 <dongyun.lee@daimresearch.com>
2022-07-08 17:24:54 +0900

    fix: restored home color att

--- OMSWeb/frontend/packages/oms-track-monitor/src/Root/OmsTrackMonitor.ce.vue
+++ OMSWeb/frontend/packages/oms-track-monitor/src/Root/OmsTrackMonitor.ce.vue
@@ -26,11 +26,9 @@ import {
   setElementRect,
   elementRectInfo,
 } from '../MapObjects/map/elementRect'
-import { rotate } from '../MapObjects/rotate/rotate'
 import { scaleInfo } from '../MapObjects/scale/scale'
 import { rotationInfo } from '../MapObjects/rotate/rotate'
 import { exposed } from './exposed'
-
 /**
  *  https://v3.vuejs.org/api/sfc-script-setup.html#typescript-only-features
  *
@@ -44,34 +42,34 @@ const props = defineProps<{
   // enums
   viewMode: ViewMode // not implemented
   mapType: MapType // not implemented
-
   // rect
   width: Numberlish
   height: Numberlish
-
   // scale
   vehicleSize: Numberlish
   segmentWidth: Numberlish
   segmentDirectionSize: Numberlish
   stationMargin: Numberlish
   bufferMargin: Numberlish
-
   // visible
   isMinimapVisible: Boolish
   isVehicleLineVisible: Boolish
   isSegmentDirectionVisible: Boolish
   isPointLabelVisible: Boolish
+  isPointHomeVisible: Boolish
   isStationVisible: Boolish
   isBufferVisible: Boolish
+  isZcuVisible: Boolish
   isGroupVisible: Boolish
   isClusterVisible: Boolish
-
   // color
   backgroundColor: Stringlish
   stationColor: Stringlish
+  stationDisabledColor: Stringlish
   bufferColor: Stringlish
   bufferDisabledColor: Stringlish
   pointColor: Stringlish
+  homeColor: Stringlish
   normalSegmentColor: Stringlish
   disabledSegmentColor: Stringlish
   segmentDirectionColor: Stringlish
@@ -82,25 +80,20 @@ const props = defineProps<{
   cargoFullColor: Stringlish
   cargoUnloadingColor: Stringlish
 }>()
-
 const propRefs = toRefs(props)
-
 interface Emits extends RootEmits {}
 const emit = defineEmits<Emits>()
 provide(RootEmitInjectionKey, readonly(emit))
 // HOW TO USE
 // const emit = inject<RootEmits>(RootEmitInjectionKey)!
-
 // const viewMode = ref<ViewMode>('PUBLIC')
 // const mapType = ref<MapType>('DB');
-
 // rect
 watch([propRefs.width, propRefs.height], () => {
   const width = parseNumberProp(0, props.width)
   const height = parseNumberProp(0, props.height)
   if (width > 0 && height > 0) setElementRect(width, height)
 })
-
 // scale
 watch(propRefs.vehicleSize, (n) => {
   updateScaleStyle(
@@ -132,7 +125,6 @@ watch(propRefs.bufferMargin, (n) => {
     parseNumberProp(ScaleDefault.bufferMargin, n)
   )
 })
-
 // visibility
 watch(propRefs.isMinimapVisible, (b) => {
   updateVisibleStyle('minimap', parseBooleanProp(true, b))
@@ -140,11 +132,17 @@ watch(propRefs.isMinimapVisible, (b) => {
 watch(propRefs.isPointLabelVisible, (b) => {
   updateVisibleStyle('pointLabel', parseBooleanProp(true, b))
 })
+watch(propRefs.isPointHomeVisible, (b) => {
+  updateVisibleStyle('pointHome', parseBooleanProp(true, b))
+})
 watch(propRefs.isStationVisible, (b) => {
   updateVisibleStyle('station', parseBooleanProp(true, b))
 })
 watch(propRefs.isBufferVisible, (b) => {
   updateVisibleStyle('buffer', parseBooleanProp(true, b))
+})
+watch(propRefs.isZcuVisible, (b) => {
+  updateVisibleStyle('zcu', parseBooleanProp(true, b))
 })
 watch(propRefs.isGroupVisible, (b) => {
   updateVisibleStyle('group', parseBooleanProp(true, b))
@@ -158,7 +156,6 @@ watch(propRefs.isVehicleLineVisible, (b) => {
 watch(propRefs.isSegmentDirectionVisible, (b) => {
   updateVisibleStyle('segmentDirection', parseBooleanProp(true, b))
 })
-
 const selfElement = ref<HTMLDivElement>()
 const shadowRoot = computed(
   () => selfElement.value?.parentNode as ShadowRoot | null | undefined
@@ -166,15 +163,12 @@ const shadowRoot = computed(
 provide('shadowRoot', readonly(shadowRoot))
 // HOW TO USE
 // const shadowRoot = inject<Ref<ShadowRoot>>('shadowRoot')
-
 // # development
 // const exposedProxy = makeFsProxy(exposed)
 // defineExpose(exposedProxy)
-
 // # production
 defineExpose(exposed)
 </script>
-
 <template>
   <div
     ref="selfElement"
@@ -196,14 +190,18 @@ defineExpose(exposed)
     <Minimap class="absolute" style="bottom: 45px; left: 45px" />
     <div
       class="absolute flex flex-row"
-      style="padding: unset; bottom: 10px; right: 10px"
+      style="
+        padding: unset;
+        bottom: 10px;
+        right: 10px;
+        align-items: center;
+      "
     >
       <ScaleBar />
       <ScreenDetail />
     </div>
   </div>
 </template>
-
 <style>
 /* dynamic css */
 /* Configurable Color Start */
@@ -212,13 +210,16 @@ defineExpose(exposed)
     'parseStringProp(ColorDefault.background, props.backgroundColor)'
   );
 }
-
 #station-layer .station .station-path {
   stroke: v-bind(
     'parseStringProp(ColorDefault.station, props.stationColor)'
   );
 }
-
+#station-layer .station[data-disabled='true' i] .station-path {
+  stroke: v-bind(
+    'parseStringProp(ColorDefault.stationDisabled, props.stationDisabledColor)'
+  );
+}
 #buffer-layer .buffer .buffer-path {
   stroke: v-bind(
     'parseStringProp(ColorDefault.buffer, props.bufferColor)'
@@ -229,24 +230,23 @@ defineExpose(exposed)
     'parseStringProp(ColorDefault.bufferDisabled, props.bufferDisabledColor)'
   );
 }
-
 #point-layer .point .point-path {
   stroke: v-bind('parseStringProp(ColorDefault.point, props.pointColor)');
   fill: v-bind('parseStringProp(ColorDefault.point, props.pointColor)');
 }
-
+#point-layer .point .home .home-path {
+  fill: v-bind('parseStringProp(ColorDefault.home, props.homeColor)');
+}
 #segment-layer .segment .segment-path {
   stroke: v-bind(
     'parseStringProp(ColorDefault.normalSegment, props.normalSegmentColor)'
   );
 }
-
 #disabled-segment-layer .segment .segment-path {
   stroke: v-bind(
     'parseStringProp(ColorDefault.disabledSegment, props.disabledSegmentColor)'
   );
 }
-
 #disabled-segment-layer
   .segment[data-is-disabled-by-mtl='true' i]
   .segment-path {
@@ -254,7 +254,6 @@ defineExpose(exposed)
     'parseStringProp(ColorDefault.disabledSegment, props.normalSegmentColor)'
   );
 }
-
 #segment-layer .segment .segment-direction,
 #disabled-segment-layer .segment .segment-direction {
   stroke: v-bind(
@@ -264,28 +263,24 @@ defineExpose(exposed)
     'parseStringProp(ColorDefault.segmentDirection, props.segmentDirectionColor)'
   );
 }
-
 #vehicle-layer .vehicle-symbol .vehicle-mode-path {
   /* mode === none */
   fill: v-bind(
     'parseStringProp(ColorDefault.noneModeVehicle, props.noneModeVehicleColor)'
   );
 }
-
 #vehicle-layer .vehicle-symbol[data-mode='A' i] .vehicle-mode-path {
   /* mode === auto */
   fill: v-bind(
     'parseStringProp(ColorDefault.autoModeVehicle, props.autoModeVehicleColor)'
   );
 }
-
 #vehicle-layer .vehicle-symbol[data-mode='M' i] .vehicle-mode-path {
   /* mode === manual */
   fill: v-bind(
     'parseStringProp(ColorDefault.manualModeVehicle, props.manualModeVehicleColor)'
   );
 }
-
 #vehicle-layer .cargo-loading {
   fill: v-bind(
     'parseStringProp(ColorDefault.cargoLoading, props.cargoLoadingColor)'
@@ -302,48 +297,45 @@ defineExpose(exposed)
   );
 }
 /* Configurable Color End */
-
 /* Configurable Visibility Start */
 #vehicle-layer .line {
   visibility: v-bind(
     "visibleStylesInfo.vehicleLine ? 'initial' : 'hidden'"
   );
 }
-
 #segment-layer .segment-direction,
 #disabled-segment-layer .segment-direction {
   visibility: v-bind(
     "visibleStylesInfo.segmentDirection ? 'initial' : 'hidden'"
   );
 }
-
 #station-layer {
   visibility: v-bind("visibleStylesInfo.station ? 'initial' : 'hidden'");
 }
-
 #buffer-layer {
   visibility: v-bind("visibleStylesInfo.buffer ? 'initial' : 'hidden'");
 }
-
+#zcu-layer {
+  visibility: v-bind("visibleStylesInfo.zcu ? 'initial' : 'hidden'");
+}
 #cluster-layer {
   visibility: v-bind("visibleStylesInfo.cluster ? 'initial' : 'hidden'");
 }
-
 #minimap-container {
   visibility: v-bind("visibleStylesInfo.minimap ? 'initial' : 'hidden'");
 }
-
 #point-layer .label {
   visibility: v-bind(
     "visibleStylesInfo.pointLabel ? 'initial' : 'hidden'"
   );
 }
-
+#point-layer .home {
+  visibility: v-bind("visibleStylesInfo.pointHome ? 'initial' : 'hidden'");
+}
 .group-shadow {
   visibility: v-bind("visibleStylesInfo.group ? 'initial' : 'hidden'");
 }
 /* Configurable Visibility End */
-
 /* Configurable Scale Start */
 #vehicle-layer .vehicle-symbol .scale-and-reverse-rotate {
   /* transform */
@@ -352,43 +344,37 @@ defineExpose(exposed)
     )
     rotate(var(--reverse-rotation-degree));
 }
-
 #segment-layer .segment-path,
 #disabled-segment-layer .segment-path {
   stroke-width: v-bind('scaleStylesInfo.segmentWidth');
 }
-
 #segment-layer .focus,
 #disabled-segment-layer .focus {
   stroke-width: v-bind('scaleStylesInfo.segmentWidth * 3');
 }
-
 #cluster-layer .cluster {
   stroke-width: v-bind('scaleStylesInfo.segmentWidth * 2.2');
 }
 /* Configurable Scale End */
-
 .scale-and-reverse-rotate {
   --reverse-rotation-degree: v-bind('`${rotationInfo * (-1)}deg`');
   --mm-per-pixel: v-bind('scaleInfo.mmPerPixel');
 }
-
 :hover {
   --filter-size: v-bind('`${scaleInfo.mmPerPixel * 10}px`');
 }
 </style>
-
 <!-- https://v3.vuejs.org/api/sfc-spec.html#src-imports -->
 <!-- https://github.com/vuejs/vue-next/issues/4662 -->
 <!-- https://v3.vuejs.org/guide/web-components.html#sfc-as-custom-element -->
-
 <!-- MapObjects -->
 <!-- Map > common -->
 <style src="src/MapObjects/styles/pan.css"></style>
 <style src="src/MapObjects/styles/rotate.css"></style>
 <style src="src/MapObjects/styles/will-change.css"></style>
 <!-- <style src="./MapObjects/styles/will-change.css"></style> -->
-
+<!-- Map > Scale -->
+<style src="src/MapObjects/scale/styles/transform.css"></style>
 <!-- TrackObjects -->
 <!-- Track > common -->
 <style src="src/TrackObjects/styles/focus.css"></style>
@@ -445,6 +431,5 @@ defineExpose(exposed)
 <style src="src/styles/sheets/utility.css"></style>
 <style src="src/styles/sheets/invert.css"></style>
 <style src="src/styles/sheets/fixed-scale.css"></style>
-
 <!-- Plan B -->
 <!-- https://stackoverflow.com/questions/69797635/how-do-i-create-a-vue-3-custom-element-including-child-component-styles -->
