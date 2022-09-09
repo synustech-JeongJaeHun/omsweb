920bba9e643c727fcc079e4ecf67916d01bfeabd
ygchoi_daim <ygchoi@daimresearch.com>
2022-09-10 03:50:43 +0900

    Support Popup Cps Reference Value Dialog at Cps Table

---
+++ OMSWeb/frontend/packages/app/src/app/modules/shared/dialogs/cps-reference-dialog.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnDestroy } from '@angular/core'
+import { Subject } from 'rxjs'
+
+export interface PeriodicElement {
+  position: number,
+  name: string;
+  normal: string;
+  warning: string;
+  fault: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Voltage', normal: '265 ~ 350 V', warning: '350 ~ 430 V', fault: '> 430 V' },
+  { position: 2, name: 'Current IGBT', normal: '0 ~ 130 A', warning: '130 ~ 140 V', fault: '> 140 A' },
+  { position: 3, name: 'Current Track', normal: '70 ~ 85 A', warning: '85 ~ 95 A', fault: '> 95 A' },
+  { position: 4, name: 'Temp Radiator', normal: '0 ~ 60 ℃', warning: '60 ~ 80 ℃', fault: '> 80 ℃' },
+  { position: 5, name: 'Temp Internal', normal: '0 ~ 35 ℃', warning: '35 ~ 40 ℃', fault: '> 40 ℃' },
+];
+
+@Component({
+	selector: 'oms-cps-reference-dialog',
+	templateUrl: './cps-reference-dialog.component.html',
+	styleUrls: ['./cps-reference-dialog.component.scss'],
+})
+export class CpsReferenceDialogComponent implements OnDestroy {
+
+    displayedColumns: string[] = ['position', 'name', 'normal', 'warning', 'fault'];
+    cpsDataSource = ELEMENT_DATA;
+
+    private destroy$: Subject<void> = new Subject<void>()
+
+	constructor(
+	) {
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next()
+		this.destroy$.complete()
+	}
+}
