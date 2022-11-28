c8d29dba251a4d76f2768fd9c92132ffad309673
ygchoi_daim <ygchoi@daimresearch.com>
2022-11-28 19:54:24 +0900

    Upgrade to V1.0.0.9

--- OMSWeb/Models/Entities/AlarmEntity.cs
+++ OMSWeb/Models/Entities/AlarmEntity.cs
@@ -2,34 +2,34 @@ using System;
 
 namespace OMSWeb.Models.Entities
 {
-  public class AlarmEntity
-  {
-    public int Id { get; set; }
-    public DateTime Time { get; set; }
-    public int ErrorCode { get; set; }
-    public int VehicleId { get; set; }
-    public string VehicleLogicalId { get; set; }
-    public DateTime? TimeResolved { get; set; }
-  }
+    public class AlarmEntity
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public int ErrorCode { get; set; }
+        public int VehicleId { get; set; }
+        public string VehicleLogicalId { get; set; }
+        public DateTime? TimeResolved { get; set; }
+    }
 
-  public class AlarmHistory : AlarmEntity
-  {
-    public TimeSpan? Age { get; set; }
-    public int Level { get; set; }
-    public string Description { get; set; }
-    public string Action { get; set; }
-    public string Note { get; set; }
-    public string Cause { get; set; }
-    public bool Cleared { get; set; }
-    public string Current { get; set; }
-  }
+    public class AlarmHistory : AlarmEntity
+    {
+        public TimeSpan? Age { get; set; }
+        public int Level { get; set; }
+        public string Description { get; set; }
+        public string Action { get; set; }
+        public string Note { get; set; }
+        public string Cause { get; set; }
+        public bool Cleared { get; set; }
+        public string Current { get; set; }
+    }
 
-  public class VehicleError
-  {
-    public int Id { get; set; }
-    public int Level { get; set; }
-    public string Description { get; set; }
-    public string Cause { get; set; }
-    public string Action { get; set; }
-  }
+    public class VehicleError
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public string Description { get; set; }
+        public string Cause { get; set; }
+        public string Action { get; set; }
+    }
 }
