e4f5cbbb90ca19a39f10d284f7681b9e1d3c8e8a
ygchoi_daim <ygchoi@daimresearch.com>
2023-02-07 12:44:28 +0900

    Modify Log Format

--- OMSWeb/Logger/Log.cs
+++ OMSWeb/Logger/Log.cs
@@ -6,12 +6,30 @@ using OMSWeb.OMSSettings;
 
 namespace OMSWeb.Logger
 {
+    public enum LogType : byte
+    {
+        SYSTEM = 0,
+        HOST = 1,
+        ORDER = 2,
+        VHL = 3,
+    }
+
+    public enum LogEventLevel
+    {
+        Verbose = Serilog.Events.LogEventLevel.Verbose,
+        Debug = Serilog.Events.LogEventLevel.Debug,
+        Information = Serilog.Events.LogEventLevel.Information,
+        Warning = Serilog.Events.LogEventLevel.Warning,
+        Error = Serilog.Events.LogEventLevel.Error,
+        Fatal = Serilog.Events.LogEventLevel.Fatal,
+    }
+
     public class Log 
     {
         public readonly static string DATE_SPLITTER = "yyyy-MM-dd";
         static Serilog.Core.Logger logger;
         static string rootPath;
-        static long seq = 0;
+        static uint seq = 0;
         static private bool initialized = false;
 
         public static bool Initialize() 
@@ -46,7 +64,7 @@ namespace OMSWeb.Logger
                             .Enrich.WithProperty("mapInfo", $"0;{DateTime.Now.ToString(DATE_SPLITTER)}")
                             .WriteTo.Async(writeTo => writeTo.Map("mapInfo", $"0;{DateTime.Now.ToString(DATE_SPLITTER)}"
                                 , (mapInfo, wt) => wt.File(string.Format("{0}/{1}/WEB/{2}-.log", rootPath, mapInfo.Split(";")[1], ((LogType)Convert.ToInt32(mapInfo.Split(";")[0])).ToString())
-                                , outputTemplate: "{Timestamp:yyyy-MM-dd}#{Timestamp:HH:mm:ss.fff}#{Level}#{Message:lj}{NewLine}{Exception}"
+                                , outputTemplate: "[{Timestamp:yyyy-MM-dd} {Timestamp:HH:mm:ss.fff}]{Message:lj}{NewLine}{Exception}"
                                 , rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: maximumFileSize, retainedFileCountLimit: null)))
                             .CreateLogger();
 
@@ -62,14 +80,48 @@ namespace OMSWeb.Logger
             Log.Dispose();
         }
 
+        public static string GetSequence()
+        {
+            return (seq++ & 0xFFFF).ToString("X4");
+        }
+
+        public static string GetModuleName(LogType type)
+        {
+            string module = string.Empty;
+            switch ((int)type)
+            {
+                case (int)LogType.SYSTEM: module = "SYSTEM__"; break;
+                case (int)LogType.HOST: module = "HOST____"; break;
+                case (int)LogType.ORDER: module = "ORDER___"; break;
+                case (int)LogType.VHL: module = "VHL_____"; break;
+            }
+            return module;
+        }
+
+        public static string GetLevel(LogEventLevel level)
+        {
+            switch (level)
+            {
+                case LogEventLevel.Debug: return "DEBG";
+                case LogEventLevel.Information: return "INFO";
+                case LogEventLevel.Warning: return "WARN";
+                case LogEventLevel.Error: return "EROR";
+                case LogEventLevel.Fatal: return "FATL";
+                default: return "INFO";
+            }
+        }
+
+        public static string GetOrderID(string orderId)
+        {
+            return orderId;
+        }
+
         public static void FilePrint(LogType type, LogEventLevel level, string format, params object[] args)
         {
-            if (!initialized)
-                return;
-
-            format = string.Format(format, args);
-            format = string.Format("{0}#{1}#{2}", (seq++).ToString("X8"), type.ToString(), format);
-            logger.ForContext("mapInfo", $"{Convert.ToInt32(type)};{DateTime.Now.ToString(DATE_SPLITTER)}").Write((Serilog.Events.LogEventLevel)level, format);
+            if (!initialized) return;
+            string logText = string.Format(format, args);
+            string message = $"[{GetSequence()}][{GetModuleName(type)}][{GetLevel(level)}][{GetOrderID(string.Empty)}] {logText}";
+            logger.ForContext("mapInfo", $"{Convert.ToInt32(type)};{DateTime.Now.ToString(DATE_SPLITTER)}").Write(level, message);
         }
 
         public static void Dispose() 
@@ -79,23 +131,5 @@ namespace OMSWeb.Logger
 
             logger.Dispose();
         }
-    }
-
-    public enum LogType : byte 
-    {
-        SYSTEM = 0,
-        HOST = 1,
-        ORDER = 2,
-        VHL = 3,
-    }
-
-    public enum LogEventLevel 
-    {
-        Verbose = Serilog.Events.LogEventLevel.Verbose,
-        Debug = Serilog.Events.LogEventLevel.Debug,
-        Information = Serilog.Events.LogEventLevel.Information,
-        Warning = Serilog.Events.LogEventLevel.Warning,
-        Error = Serilog.Events.LogEventLevel.Error,
-        Fatal = Serilog.Events.LogEventLevel.Fatal,
     }
 }
