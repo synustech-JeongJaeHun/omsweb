ef439def1417e2acd66af738fe78a18e297ff202
ygchoi_daim <ygchoi@daimresearch.com>
2023-03-06 23:54:34 +0900

    Modify Trend Utilization & report utilization

--- OMSWeb/Repositories/ReportTrendRepository.cs
+++ OMSWeb/Repositories/ReportTrendRepository.cs
@@ -300,7 +300,6 @@ namespace OMSWeb.Repositories
                                     600 * (
                                             select count(*)::int 
                                             from vehicles 
-                                            where rail_in = true
                                         )::decimal
                                     )
                             ) * 100
@@ -322,7 +321,7 @@ namespace OMSWeb.Repositories
                                     time_assigned,
                                     time_completed
                                 from orders10m
-                                where (time_aborted is null or time_failed is null) and vehicle_id is not null
+                                where (time_aborted is null and time_failed is null) and vehicle_id is not null
                             ) fol
                         ) ol
                     ) times
