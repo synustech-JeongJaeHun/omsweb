942b26f7d4368012d874b8eeea4c5493547f253e
snkwak <snkwak@daimresearch.com>
2023-08-09 14:14:40 +0900

    cache max-age set 0

--- OMSWeb/Startup.cs
+++ OMSWeb/Startup.cs
@@ -234,7 +234,7 @@ namespace OMSWeb
             {
                 OnPrepareResponse = context =>
                 {
-                    context.Context.Response.Headers.Add("Cache-Control", "public, max-age=2592000");
+                    context.Context.Response.Headers.Add("Cache-Control", "public, max-age=0");
                     context.Context.Response.Headers.Add("Expires", "-1");
                 }
             });
@@ -245,7 +245,7 @@ namespace OMSWeb
                 {
                     OnPrepareResponse = context =>
                     {
-                        context.Context.Response.Headers.Add("Cache-Control", "public, max-age=2592000");
+                        context.Context.Response.Headers.Add("Cache-Control", "public, max-age=0");
                         context.Context.Response.Headers.Add("Expires", "0");
                         context.Context.Response.Headers.Add("Pragma", "no-cache");
                     }
