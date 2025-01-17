f1bdfba26cf9c0f24b43bfb77a7bf07a695769f6
BK.lee <bkl@synustech.com>
2025-01-17 11:30:30 +0900

    버전 로그 남기는 방식 변경 (서비스로 실행 시 실행 안 되는 문제)

--- OMSWeb/Startup.cs
+++ OMSWeb/Startup.cs
@@ -41,14 +41,9 @@ namespace OMSWeb
             OmsConfiguration();
 
             Log.Initialize();
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
 
-            string filePath = Process.GetCurrentProcess().MainModule.FileName;
-            string fileVersion = FileVersionInfo.GetVersionInfo(filePath).FileVersion;
-            string fileName = Path.GetFileNameWithoutExtension(filePath);
-            string title = fileName + " " + fileVersion;
-            Console.Title = title;
-
-            Log.FilePrint(LogType.SYSTEM, LogEventLevel.Debug, $"{title} SERVER start at: {DateTimeOffset.Now}");
+            Log.FilePrint(LogType.SYSTEM, LogEventLevel.Debug, $"OMSWeb SERVER {version} start at: {DateTimeOffset.Now}");
         }
 
         public IConfiguration Configuration { get; }
