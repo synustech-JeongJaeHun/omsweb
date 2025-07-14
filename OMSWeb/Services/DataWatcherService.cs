5dd767af47bf8f7515d920ee750485672ea8b5cc
BK.lee <bkl@synustech.com>
2025-07-14 18:03:39 +0800

    BackEnd 에서 DB Disconnect 발생 시 재연결 하도록 수정

--- OMSWeb/Services/DataWatcherService.cs
+++ OMSWeb/Services/DataWatcherService.cs
@@ -6,73 +6,132 @@ using Microsoft.Extensions.Hosting;
 using Npgsql;
 using OMSWeb.OMSSettings;
 
-#pragma warning disable 0168
-
 namespace OMSWeb.Services
 {
     public class DataWatcherService : BackgroundService
     {
+        private bool NeedReConnect = false;
         private NpgsqlConnection trackConn;
         private PushService _pushSvc;
         protected string connectionStringTrack;
 
-        public DataWatcherService(IConfiguration configuration, PushService pushSvc)
+        public DataWatcherService(IConfiguration _, PushService pushSvc)
         {
-            /*
-            // appsettings.json
-            //"ConnectionStrings": {
-            //    "OMS-UI": "Server=localhost;Port=5432;Database=oms_ui;User Id=oms;Password=oms;",
-            //    "OMS-Track": "Server=localhost;Port=5432;Database=semioht;User Id=oms;Password=oms;"
-            //},
-            */
-            // get default AppSetting.json
-            //this.connectionStringTrack = configuration.GetConnectionString("OMS-Track");
+            AppConfig.GetConnectStrFromOmsSettings(out string _, out string connectTrackStr);
+            connectionStringTrack = connectTrackStr;
 
-            // get config from oms_settings.ini
-            AppConfig.GetConnectStrFromOmsSettings(out string connectUiStr, out string connectTrackStr);
-            this.connectionStringTrack = connectTrackStr;
-
-            this.trackConn = new NpgsqlConnection(this.connectionStringTrack);
-            this._pushSvc = pushSvc;
+            CreateDBConnect();
+            _pushSvc = pushSvc;
         }
 
+        protected bool IsNotConnectedDB()
+        {
+            return
+                trackConn == null ||
+                trackConn.State == System.Data.ConnectionState.Closed ||
+                trackConn.State == System.Data.ConnectionState.Broken;
+        }
+
+        protected bool CreateDBConnect()
+        {
+            try
+            {
+                if (IsNotConnectedDB() || NeedReConnect)
+                {
+                    trackConn = new NpgsqlConnection(connectionStringTrack);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        protected bool DBOpen()
+        {
+            try
+            {
+                trackConn.Open();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        protected bool RegisterDBNotification()
+        {
+            try
+            {
+                trackConn.Notification += NotificationReceivedAsync;
+
+                using var cmd = trackConn.CreateCommand();
+                cmd.CommandText = "LISTEN monitor_track";
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            this.trackConn.Notification += this.NotificationReceivedAsync;
+            DBOpen();
+            RegisterDBNotification();
 
             Console.WriteLine("### Data Watcher started");
             while (!stoppingToken.IsCancellationRequested)
             {
                 try
                 {
-                    if (trackConn.State == System.Data.ConnectionState.Closed)
-                    {
-                        this.trackConn.Open();
-
-                        using (var cmd = trackConn.CreateCommand())
-                        {
-                            cmd.CommandText = "LISTEN monitor_track";
-                            cmd.ExecuteNonQuery();
-                        }
-                    }
-
                     await trackConn.WaitAsync();
                 }
-                catch (System.Exception e)
+                catch (Exception e)
                 {
                     Console.WriteLine($"@@@ Error : {e}");
-                    Thread.Sleep(10);
+                    Thread.Sleep(1000);
+
+                    NeedReConnect = true;
+                    if (CreateDBConnect())
+                    {
+                        if (DBOpen())
+                        {
+                            if (RegisterDBNotification())
+                            {
+                                NeedReConnect = false;
+                                Console.WriteLine($"@@@ Reconnect Success.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"@@@ Reconnect Register Notification Fail.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"@@@ Reconnect Open Fail.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"@@@ Reconnect Create Fail.");
+                    }
                 }
             }
         }
 
         public override void Dispose()
         {
-            if (this.trackConn != null && this.trackConn.State != System.Data.ConnectionState.Closed)
+            if (trackConn != null && trackConn.State != System.Data.ConnectionState.Closed)
             {
-                this.trackConn.Close();
-                this.trackConn.Dispose();
+                trackConn.Close();
+                trackConn.Dispose();
             }
             Console.WriteLine("## Watcher disposed.");
             base.Dispose();
@@ -82,23 +141,12 @@ namespace OMSWeb.Services
         {
             var now = DateTime.Now;
             var ts = now.Ticks;
-            // if ((ts / (10 ^ 7)) % 10 == 0)
-            // {
-            //   Console.ForegroundColor = ConsoleColor.DarkGray;
-            //   Console.WriteLine($"[PUSH] {0,8:N2} ~\t00 event => {now}");
-            //   Console.ResetColor();
-            // }
-            // var payload = e.Payload;
-            // if (payload.Contains("vehicles"))
-            // {
-            //   Console.WriteLine($"# DB Notify >> {e.Payload}");
-            // }
-            // return;
+
             try
             {
-                await this._pushSvc.PushWatcherEventAsync(ts, e.Payload);
+                await _pushSvc.PushWatcherEventAsync(ts, e.Payload);
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine($"[Notification Error] {ex}");
             }
